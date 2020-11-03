321acb3c5844c710aed99c9686b98638a9a7a9c7
Richard Hamilton <richard.hamilton@gm.com>
2020-11-02 19:07:11 -0500

    Upload student template code

--- beginners/1. intro to python/7. sum_calculator_wrong.py
+++
@@ -1,9 +0,0 @@
-#get user inputs
-first_num = input('Please enter first number: ')
-second_num = input('Please enter second number: ')
-
-#sum the two numbers
-result = first_num + second_num
-
-#output the result to the user
-print('The sum of the two numbers is', result)
