3848c624a8f89bfc51aa473606f057bdf176598c
Nancy Du <nancydu9@gmail.com>
2020-11-21 21:01:57 -0500

    updated template code for lasers

--- advance/mathstronauts_game_development/Week10_lasers_TEMPLATE.py
+++ advance/mathstronauts_game_development/Week10_lasers_TEMPLATE.py
@@ -1,45 +1,20 @@
 # SEV Advanced - Week 10 Adding Shooting Features TEMPLATE
-# Students will learn to use lists to fire and keep track of lasers
 
 # COPY AND PASTE YOUR CODE HERE:
 
 
+# activating lasers (template code)____________________________________________________________________________________________
+player.laserActive = 1  # add laserActive status to the player
 
-
-# ________________________CREATE UPDATELASERS FUNCTION__________________________
-def updateLasers():
-    global lasers, score 
-    x_speed = __ # add laser speed
-    for i in range(len(lasers)):
-        lasers[i].x += x_speed
-        if lasers[i].right < __:  # if laser moves off the screen, remove if from our game list
-            lasers[i].status = 1
-
-        # check for collisions with lasers (just debris and satellites)
-        # if debris collide with lasers
-            # mark laser for removal
-            # reinitialize debris
-            # change score
-        # elif satellite collide with lasers
-            # mark laser for removal
-            # reinitialize satellite
-            # change score
-
-    lasers = mathstropy.pgzero.listCleanup(lasers)  # this helps make sure our lists are working properly
-
-# activating lasers (template code - DO NOT DELETE)
-player.laserActive = 1
-
-def makeLaserActive():
+def makeLaserActive():  # when called, this function will make lasers active again
     global player
     player.laserActive = 1
 
-def fireLasers(sprite):
+def fireLasers(laser):
     if player.laserActive == 1:  # active status is used to prevent continuous shoot when holding space key
         player.laserActive = 0
         clock.schedule(makeLaserActive, 0.2)  # schedule an event (function, time afterwhich event will occur)
-        sounds.laserfire02.play()
-        lasers.append(sprite)
-        lasers[len(lasers) - 1].status = 0
-    
+        sounds.laserfire02.play()  # play sound effect
+        lasers.append(laser)  # add laser to lasers list
+        
 pgzrun.go()
