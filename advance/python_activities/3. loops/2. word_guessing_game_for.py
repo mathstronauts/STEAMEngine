4e135d7036ba22912812916d22a9a5d10b3663d0
Nancy Du <nancydu9@gmail.com>
2021-03-22 03:16:47 -0400

    minor update

--- advance/python_activities/3. loops/2. word_guessing_game_for.py
+++ advance/python_activities/3. loops/2. word_guessing_game_for.py
@@ -12,9 +12,9 @@ print("You will have", str(chances), "chances to guess.")
 print(hint)
 
 for i in range(chances):  # the for loop will run 3 times, the upper limit is one above the highest number we want to count to
-    guess = input("Enter guess: ")
+    guess = input("Enter guess: ")  # 
     
-    if guess.lower() != random_word:
+    if guess.lower() != random_word:  # add this after, simple for loop just asking for guess
         print("Sorry, guess incorrect.")
     else:
         print("You won!")
